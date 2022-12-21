d20f8ecfa20d9020c63dd7997efa8ed685f04798
helene-cezard <helene.cezard@gmail.com>
2022-12-21 17:54:26 +0100

    item page

--- app/views/item.tpl.php
+++ app/views/item.tpl.php
@@ -3,15 +3,17 @@
 <div class="product">
     <img class="illustration" src="<?= $product->getPicture() ?>" alt="Photo de <?= $product->getName() ?>">
     <div class="product_details">
-        <p class="error"><?= $connectionError ?></p>
+        <?php if(isset($connectionError)) : ?>
+            <p class="error"><?= $connectionError ?></p>
+        <?php endif ?>
         <p class="description"><?= $product->getDescription() ?></p>
-        <form action="" method="POST">
-            <label for="quantity">Quantité</label>
-            <input type="number" max="5" name="quantity" value="1" <?= $product->getStatus() == 1 ? "" : "disabled" ?>>
+        <form class="order-form" action="" method="POST">
+            <label class="quantity-label" for="quantity">Quantité</label>
+            <input class="quantity-input" type="number" max="5" name="quantity" value="1" <?= $product->getStatus() == 1 ? "" : "disabled" ?>>
             <p class="error"><?= $quantityError ?></p>
             <input type="hidden" name="productId" value="<?= $product->getId() ?>">
             <p>Disponibilité : <?= $product->getStatus() == 1 ? "En stock" : "Indisponible"; ?></p>
-            <button type="submit">Ajouter au panier</button>
+            <button class="submit" type="submit">Ajouter au panier</button>
         </form>
     </div>
 </div>
